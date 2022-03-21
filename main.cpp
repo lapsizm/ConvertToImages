6b2c0f1ff4c8d7557837120bc74f7b5ed3f0ea45
lapsizm <78648610+lapsizm@users.noreply.github.com>
2022-03-21 21:34:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,37 @@ void get_filename(const std::string &path) {
         cout << file.path().filename() << endl;
 }
 
+//функция для создания архива при наличии myfolder
+void archive(const std::string &path){
+    using std::filesystem::recursive_directory_iterator;
+    std::string new_file("../new_file.dat");
+    std::ofstream fout(new_file);
+    int temp = path.length() - 1;
+    while(temp > 0){
+        if(path[temp] == '/'){
+            break;
+        }
+        --temp;
+    }
+    std::string name_dir = path.substr(temp + 1, path.length());
+    fout << name_dir << '\0';
+
+    for (const auto &file: recursive_directory_iterator(path)){
+        std::ifstream fin(file.path());
+        std::string file_name = file.path().filename().string();
+        fout << file_name << '\0';
+        int size = file.file_size();
+        fout.write(reinterpret_cast<char*>(&size), 4);
+        for(int i = 0; i < size; ++i){
+            char symbol;
+            fin.read(reinterpret_cast<char*>(&symbol), 1);
+            fout.write(reinterpret_cast<char *>(&symbol), 1);
+        }
+        fin.close();
+    }
+    fout.close();
+}
+
 int main(int argc, char **argv) {
     if (argc == 2) {
         std::string path = argv[1];
@@ -31,7 +62,6 @@ int main(int argc, char **argv) {
         std::string dir_name = "../";
         dir_name += temp_dir + "/";
         std::filesystem::create_directory(dir_name);
-
         while (!fin.eof()) {
             std::string file_name;
             std::getline(fin, file_name, '\0');
@@ -50,4 +80,5 @@ int main(int argc, char **argv) {
         print_all_files(dir_name);
         get_filename(dir_name);
     }
+    archive("../myfolder");
 }
